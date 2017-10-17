e2074a696e3b0ec5c5af5e1b7c30af7ab25d7bd8
Guillaume Chanson <guillaume.chns@gmail.com>
2017-10-17 16:02:00 +0200

    multiple examples from OpenStack STrace

--- Main.py
+++ Main.py
@@ -5,19 +5,28 @@ from SqlParser import SqlParser
 from SqlListener import SqlListener
 from Metrics import Metrics
 
-def main(argv):
-    input = FileStream(argv[1])
-    lexer = SqlLexer(input)
+def parse(query):
+    print(query)
+    lexer = SqlLexer(InputStream(query))
     stream = CommonTokenStream(lexer)
     parser = SqlParser(stream)
     tree = parser.sql_stmt()
-    print(tree.toStringTree(recog=parser))
+    # print(tree.toStringTree(recog=parser))
 
     walker = ParseTreeWalker()
     metrics = Metrics()
     walker.walk(metrics, tree)
     print("There are", metrics.howManyJoins(), "joins")
     print("There are", metrics.howManyTransactions(), "transactions")
+
+def main(argv):
+    i = 0
+    with open(argv[1], 'r') as file:
+        for line in file:
+            parse(line)
+            i += 1
+            if i > 10:
+                break
 
 if __name__ == '__main__':
     main(sys.argv)
